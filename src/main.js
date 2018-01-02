caf113cf7f4b637bde3a5fe589c64a055335a723
enaAdrian <ancio5@interia.pl>
2018-01-02 15:36:49 +0100

    dodany bootstrap i jquery

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import jQuery from 'jquery'
+global.jQuery = jQuery
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.config.productionTip = false
 
